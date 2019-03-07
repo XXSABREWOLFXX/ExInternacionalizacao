f5666d4375de762f943beff1e2751ad07df7d197
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-07 10:18:04 -0300

    Initial commit

---
+++ app/src/main/java/br/com/luizhenriquedefalcoRM79925/exinternacionlizacao/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.luizhenriquedefalcoRM79925.exinternacionlizacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
